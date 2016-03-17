bd8e84a496cc14bf1f09840798ed5b1ee62a7441
Charlemagne <charlemagne.doles@sjsu.edu>
2016-03-17 16:18:35 -0700

    Part 1a done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+/**** ORIGINAL CODE ****/
+
+/*
 var express = require('express')
 var app = express()
 
-/* serves all the static files*/
+//serves all the static files
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
-/*serves main page*/
+//serves main page
 app.get('/', function(request, response) {
 response.sendfile('index.html') 
 })
@@ -13,3 +16,17 @@ response.sendfile('index.html')
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+*/
+
+
+/**** PART 1A-readFileSync ****/
+
+var http = require('http');
+var fs = require('fs');
+var port = process.env.port || 8080
+
+http.createServer(function (request, response) {
+  	response.end(fs.readFileSync('index.html'),'utf8');
+  }).listen(port);
+
+console.log('Server running on port: ', port);
