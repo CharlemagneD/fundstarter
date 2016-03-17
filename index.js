80368fb0d305b52d85471156a5a751ccfd139bcf
Charlemagne <charlemagne.doles@sjsu.edu>
2016-03-17 16:22:25 -0700

    Part 2 done

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ console.log('Server running on port: ', port);
 
 /***** PART 1B-readFile ****/
 
-
+/*
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 8080
@@ -55,4 +55,41 @@ http.createServer(function (request, response) {
 	
 }).listen(port);
 console.log('Server running on port: ', port);
+*/
+/**************************************************************************/
+
+
+/**** PART 2 ****/
+/*
+Code adapted from http://www.sitepoint.com/accessing-the-file-system-in-node-js/
+*/
+
+var http = require('http');
+var fs = require("fs");
+var fileName = "index.html";
+var port = process.env.PORT || 8080
+
+http.createServer(function (request, response) { 
+	fs.exists(fileName, function(exists) {
+	  if (exists) {
+	    fs.stat(fileName, function(error, stats) {
+	      fs.open(fileName, "r", function(error, fd) {
+	        var buffer = new Buffer(stats.size);
+
+	        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+	          var data = buffer.toString("utf8", 0, buffer.length);
+	          response.writeHead(200, {'Context-Type':'text/html'});
+			  response.end(data,'utf8');
+	          fs.close(fd);
+	        });
+	      });
+	    });
+	  }
+	});
+}).listen(port);
+
+console.log('Server running on port: ', port)
+
+
+
 
