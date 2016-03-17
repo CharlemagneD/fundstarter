27d3c5ac5ffc2bcb95834f427758cd6bf544bd8e
Charlemagne <charlemagne.doles@sjsu.edu>
2016-03-17 16:20:44 -0700

    Part 1b done

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 */
-
+/**************************************************************************/
 
 /**** PART 1A-readFileSync ****/
 
+/*
 var http = require('http');
 var fs = require('fs');
 var port = process.env.port || 8080
@@ -30,3 +31,28 @@ http.createServer(function (request, response) {
   }).listen(port);
 
 console.log('Server running on port: ', port);
+*/
+/**************************************************************************/
+
+/***** PART 1B-readFile ****/
+
+
+var http = require('http');
+var fs = require('fs');
+var port = process.env.PORT || 8080
+
+http.createServer(function (request, response) {
+	fs.readFile('index.html', function(error, content) {
+		if (error) {
+			response.writeHead(500);
+			response.end();
+		}
+		else {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(content, 'utf8');
+		}
+	});
+	
+}).listen(port);
+console.log('Server running on port: ', port);
+
